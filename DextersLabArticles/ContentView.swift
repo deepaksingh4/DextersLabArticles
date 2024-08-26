d71f8ea53d668bd9aa068cf4037727e445fc9d46
Deepak <dksinbox95@gmail.com>
2024-08-26 21:19:31 +0400

    Initial Commit

---
+++ DextersLabArticles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DextersLabArticles
+//
+//  Created by Deepak Singh on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DextersLabArticles/DextersLabArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DextersLabArticlesApp.swift
+//  DextersLabArticles
+//
+//  Created by Deepak Singh on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DextersLabArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
